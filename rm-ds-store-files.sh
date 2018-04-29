539036d4d01fc9718f549a3907b055dcae9724f8
Sean Camden <sean@seancamden.com>
2018-04-29 00:12:02 -0700

    Trying to ignore lost+found folders

--- rm-ds-store-files.sh
+++ rm-ds-store-files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-find /media/backup2011/ -name .DS_Store -exec rm {} +
-find /media/backup20131003/ -name .DS_Store -exec rm {} +
+find /media/backup2011/ ! -readable -prune -o -name .DS_Store -exec rm {} +
+find /media/backup20131003/ ! -readable -prune -o -name .DS_Store -exec rm {} +
 
